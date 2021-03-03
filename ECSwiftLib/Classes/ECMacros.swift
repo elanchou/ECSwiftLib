aaf19337a11883fad6a05d5aa0f3a84a1d9fd7d8
elanchou <lucidity95@gmail.com>
2021-03-03 15:39:07 +0800

    Remove unuseful code

--- ECSwiftLib/Classes/ECMacros.swift
+++ ECSwiftLib/Classes/ECMacros.swift
@@ -27,22 +27,3 @@ public let ECNavBarHeight_StatusHeight: ((UIViewController)-> CGFloat) = {(vc : 
     var navHeight = weakVC?.navigationController?.navigationBar.bounds.size.height ?? 0.0
     return (navHeight + ECStatusBarHeight)
 }
-
-
-// MARK: ============================================================================
-// MARK: 设置颜色
-/// 设置颜色值
-public let HexRGB:((Int) -> UIColor) = { (rgbValue : Int) -> UIColor in
-    return HexRGBAlpha(rgbValue,1.0)
-}
-
-/// 通过 十六进制与alpha来设置颜色值  （ 样式： 0xff00ff ）
-public let HexRGBAlpha:((Int,Float) -> UIColor) = { (rgbValue : Int, alpha : Float) -> UIColor in
-    return UIColor(red: CGFloat(CGFloat((rgbValue & 0xFF0000) >> 16)/255), green: CGFloat(CGFloat((rgbValue & 0xFF00) >> 8)/255), blue: CGFloat(CGFloat(rgbValue & 0xFF)/255), alpha: CGFloat(alpha))
-}
-
-
-/// 通过 red 、 green 、blue 、alpha 颜色数值
-public let RGBAlpa:((Float,Float,Float,Float) -> UIColor ) = { (r: Float, g: Float , b: Float , a: Float ) -> UIColor in
-    return UIColor.init(red: CGFloat(CGFloat(r)/255.0), green: CGFloat(CGFloat(g)/255.0), blue: CGFloat(CGFloat(b)/255.0), alpha: CGFloat(a))
-}
